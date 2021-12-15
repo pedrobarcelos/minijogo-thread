c66564eb42b9faa87e377b940aae8840b03e496e
Pedro Barcelos <pbarcelos56@icloud.com>
2021-12-15 16:51:27 -0300

    feat: versao teste

--- App.java
+++ App.java
@@ -1,81 +1,92 @@
-public class App {
-    public static void main(String[] args)throws Exception {
+import java.util.Scanner;
 
-        
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
         Jogador player1 = new Jogador(1);
         Jogador player2 = new Jogador(2);
 
-        JogadorService service1 = new JogadorService();
-        JogadorService service2 = new JogadorService();
+        //vetores de servies
+        JogadorService[] service1 = new JogadorService[9];
+        JogadorService[] service2 = new JogadorService[9];
 
+        //Gerador de "alvos"
         Gerador gerador = new Gerador();
-        
 
-        //threads para gerar os chutes
-        service1.start();
-        service2.start();
-        service1.join();
-        service2.join();
-
-        //variaveis auxiliares
+        // variaveis auxiliares
         int count = 0;
         int lastWinner = 0;
         double lastNumber = 0;
 
         //laco principal
-        while(player1.getPontos() < 5 && player2.getPontos() < 5){
-            System.out.print("\n\n");
-            
-            //lógica para modificar o valor do gerador de acordo com o ultimo vencedor
-            if(lastWinner == 1){
-                do{
+        for (int i = 0; i < 9 && player1.getPontos() < 5 && player2.getPontos() < 5; i++) {
+
+            //instanciamento de services 
+            service1[i] = new JogadorService("1", i);
+            service2[i] = new JogadorService("2", i);
+
+            //disparo de threads
+            service1[i].start();
+            service2[i].start();
+            service1[i].join();
+            service2[i].join();
+
+            // lógica para modificar o valor do gerador de acordo com o ultimo vencedor
+            if (lastWinner == 1) {
+                do {
                     gerador.sortear();
-                }while(gerador.getNumero() < lastNumber);
-            }else if(lastWinner == 2){
-                do{
+                } while (gerador.getNumero() < lastNumber);
+            } else if (lastWinner == 2) {
+                do {
                     gerador.sortear();
-                }while(gerador.getNumero() > lastNumber);
-            }else{
+                } while (gerador.getNumero() > lastNumber);
+            } else {
                 gerador.sortear();
             }
+            
 
-            //logica para mostrar na tela os resultados e calcular o atual vencedor
-            lastNumber = gerador.getNumero();
-            System.out.println("----------RODADA " + (count+1) + "---------");
-            System.out.println("ALVO : " + (int)gerador.getNumero());
-            System.out.println("Chute jogador 1 : " + (int)service1.getChute(count));
-            System.out.println("Chute jogador 2 : " + (int)service2.getChute(count) + "\n---------------------------");
-            lastWinner = calcularVencedor(service1.getChute(count), service2.getChute(count), player1, player2, gerador.getNumero());
-            System.out.println("\n-----------PLACAR----------\nJOGADOR 1: " + player1.getPontos() + " pontos\n---------------------------\nJOGADOR 2: " + player2.getPontos() + " pontos\n---------------------------");
-            count++;
+            //interface do user
+            lastWinner = calcularVencedor(service1[i].getChute(),service2[i].getChute(), player1, player2,gerador.getNumero());
+            System.out.println("----------RODADA " + (i + 1) + "---------");
+            System.out.println("ALVO : " + (int) gerador.getNumero());
+            System.out.println("Chute jogador 1 : " + (int) service1[i].getChute());
+            System.out.println("Chute jogador 2 : " + (int) service2[i].getChute() + "\n---------------------------");
+            System.out.println("\n-----------PLACAR----------\nJOGADOR 1: " + player1.getPontos()+ " pontos\n---------------------------\nJOGADOR 2: " + player2.getPontos()+ " pontos\n---------------------------");            
+            System.out.println("\nEnter para continuar");
+            String enter = scanner.nextLine();
         }
 
-        if(player1.getPontos() > player2.getPontos())System.out.println("\n\nGRANDE VENCEDOR COM " + player1.getPontos() + " PONTOS : JOGADOR 1!");
-        else System.out.println("\n\nGRANDE VENCEDOR COM " + player2.getPontos() + " PONTOS : JOGADOR 2!");
-        
+        //calcular vencedor
+        if (player1.getPontos() > player2.getPontos())
+            System.out.println("\nGRANDE VENCEDOR COM " + player1.getPontos() + " PONTOS : JOGADOR 1!");
+        else
+            System.out.println("\nGRANDE VENCEDOR COM " + player2.getPontos() + " PONTOS : JOGADOR 2!");
     }
-
-    //funcao que retorna 1 se o player 1 vencer, 2 se o player 2 vencer e 3 para um empate
+    
+    // funcao que retorna 1 se o player 1 vencer, 2 se o player 2 vencer e 3 para um
+    // empate
     private static int calcularVencedor(double chute1, double chute2, Jogador player1, Jogador player2, double alvo) {
         double proximidade1 = alvo - chute1;
-        if(proximidade1 < 0.0) proximidade1 = proximidade1 * -1;
+        if (proximidade1 < 0.0)
+            proximidade1 = proximidade1 * -1;
 
         double proximidade2 = alvo - chute2;
-        if(proximidade2 < 0.0) proximidade2 = proximidade2 * -1;
+        if (proximidade2 < 0.0)
+            proximidade2 = proximidade2 * -1;
 
-        if(proximidade1 < proximidade2){
+        if (proximidade1 < proximidade2) {
             player1.pontuar();
-            System.out.println("O jogador 1 pontuou!"+ "\n---------------------------");
+            System.out.println("\nO jogador 1 pontuou!" + "\n---------------------------\n");
             return 1;
-        }
-        else if(proximidade2 < proximidade1){
+        } else if (proximidade2 < proximidade1) {
             player2.pontuar();
-            System.out.println("O jogador 2 pontuou!"+ "\n---------------------------");
+            System.out.println("\nO jogador 2 pontuou!" + "\n---------------------------\n");
             return 2;
-        }
-        else{
-            System.out.println("Empate! Nenhum jogador pontua!"+ "\n---------------------------");
+        } else {
+            System.out.println("\nEmpate! Nenhum jogador pontua!" + "\n---------------------------\n2");
             return 3;
-        }        
+        }
     }
 }
--- JogadorService.java
+++ JogadorService.java
@@ -1,30 +1,38 @@
-
+import java.util.Scanner;
+import java.io.*;
 
 public class JogadorService extends Thread{
 
+    // scanner
+    Scanner scan = new Scanner(System.in);
+
+    //variaveis usadas
+    private String id = "";
+    private int jogada;
     Gerador gerador = new Gerador();
+    Console cnsl
+            = System.console();
     
-    private double[] chute = new double[9];
+    private double chute;
 
-    public JogadorService(){
-
+    //construtor
+    public JogadorService(String id, int jogada){
+        this.id = id;
+        this.jogada = jogada;
     }
 
+    //thread
     @Override
     public void run() {
-        for(int i = 0; i < 9; i++){
-             gerador.sortear();
-             setChute(gerador.getNumero(),i);
-        }
-       
-        
+        String chute = cnsl.readLine("Chute " + (this.jogada + 1) + " do jogador "+ this.id + ": " );
+        setChute(Integer.parseInt(chute));
+    }
+    
+    public double getChute() {
+        return chute;
     }
 
-    public double getChute(int index) {
-        return chute[index];
-    }
-
-    public void setChute(double chute, int index) {
-        this.chute[index] = chute;
+    public void setChute(double chute) {
+        this.chute = chute;
     }
 }
